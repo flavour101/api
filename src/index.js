e01319a0f445d975475c4d74e846381e2e391adb
Harmelodic <mgsmith.mail@gmail.com>
2018-09-08 19:29:41 +0100

    es6

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const config = require("./config");
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('hello world')
 })
 
