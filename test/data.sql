70b4c9932d88d00aaa76321683575ba3c4b38fb0
matt.smith <matt.smith@capgemini.com>
2018-09-30 23:45:43 +0100

    Markers in manchester

--- test/data.sql
+++ test/data.sql
@@ -15,8 +15,8 @@ INSERT INTO recipe(id, title, post_date, source, thumbnail) VALUES ('1', 'Recipe
 INSERT INTO recipe(id, title, post_date, source, thumbnail) VALUES ('2', 'Recipe 2', '2018-09-02 12:30:00.000', '/md/random.md', '/random.png');
 INSERT INTO recipe(id, title, post_date, source, thumbnail) VALUES ('3', 'Recipe 2 Unpublished', NULL, '/random.md', '/random.png');
 
-INSERT INTO review(id, title, post_date, lat, lng, source, thumbnail) VALUES ('1', 'Review 1', '2018-09-01 12:30:00.000', 0.0, 0.0,'/md/random.md', '/random.png');
-INSERT INTO review(id, title, post_date, lat, lng, source, thumbnail) VALUES ('2', 'Review 2', '2018-09-02 12:30:00.000', 20.0, 50.0,'/md/random.md', '/random.png');
+INSERT INTO review(id, title, post_date, lat, lng, source, thumbnail) VALUES ('1', 'Review 1', '2018-09-01 12:30:00.000', 53.485154, -2.235328,'/md/random.md', '/random.png');
+INSERT INTO review(id, title, post_date, lat, lng, source, thumbnail) VALUES ('2', 'Review 2', '2018-09-02 12:30:00.000', 53.485559, -2.234963,'/md/random.md', '/random.png');
 INSERT INTO review(id, title, post_date, lat, lng, source, thumbnail) VALUES ('3', 'Review 3 Unpublished', NULL, 50.0, 50.0,'/md/random.md', '/random.png');
 
 INSERT INTO image(id, reference_id, reference_type, source, post_date) VALUES ('1', '1', 'blog', '/images/banner.jpeg', '2018-09-01 12:30:00.000');
